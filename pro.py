31bacf52872f16b25c294eef78cce3d5b0d8b5a6
lana-ban <subbotina96sv@gmail.com>
2020-05-04 15:39:25 +0500

    get information about professional standard

--- pro.py
+++ pro.py
@@ -9,6 +9,25 @@ def get_html(link):
 	soup = BeautifulSoup(response.text, "html.parser")
 	return soup
 
+def get_url_function(url):
+	subfunction_information, standard_information = [], []
+
+	soup = get_html(url["url_standard"])
+	general_information = soup.find(class_="description")
+
+	for i in soup.find_all("tr"):
+		if i.find("td"):
+			if i.find("table"):
+				subfunction_information.clear()
+				labor_function = i.find("a")
+			else:
+				labor_subfunction = i.find("a")
+				subfunction_information.append({'name_labor_subfunction': labor_subfunction.get_text(), 'url_subfunction': labor_subfunction.get('href')})
+				if subfunction_information:
+					standard_information.append({'specialty': general_information, 'labor_function':{'name_labor_function': labor_function.get_text(), 'url_labor_function': labor_function.get('href'), 'labor_subfunction': subfunction_information}})
+			
+	return standard_information
+
 def specialty_in_standard(url, specialty):
 	not_empty = 0 
 	soup = get_html(url)
@@ -50,7 +69,7 @@ def prof_standard(url, specialty):
 				url = "https://ppt.ru"+ i.get("href")
 				found_standard = specialty_in_standard(url, specialty)
 				if found_standard != 0:
-					array_specialty_standard.append({'name': i.get_text(), 'url': url})
+					array_specialty_standard.append({'name_standard': i.get_text(), 'url_standard': url})
 
 	return array_specialty_standard
 
@@ -60,7 +79,12 @@ def main():
 	specialty = "Программист"
 	url_standard = "https://ppt.ru/docs/profstandarts"
 
-	print(prof_standard(url_standard, specialty))
+	array_standard = prof_standard(url_standard, specialty)
+
+	for i in array_standard:
+		standard_information = get_url_function(i)
+		print(standard_information)
+		print("==================================")
 
 	print("PROGRAM TIME: ", datetime.now() - start_time)
 
