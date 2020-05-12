a3089cb9726c4c11d62c26d47097ad6604fe7677
lana-ban <subbotina96sv@gmail.com>
2020-05-12 14:43:38 +0500

    writing data to the database

---
+++ database.py
@@ -0,0 +1,97 @@
+import pymysql
+import connectvars
+
+def database_connection(connectvars):
+	localhost, user, password, db = connectvars
+	try:
+		conn = pymysql.connect(host = localhost, database = db, user = user, password = password)
+	except:
+		conn.rollback()
+
+	return conn
+
+def insert_prof_standard(name_standard, url_standard, general_information):
+	con = database_connection(connectvars.connection())
+	cur = con.cursor()
+	try:
+		if not found_id_standard(url_standard):
+			cur.execute('INSERT prof_standard(name, url, activity) VALUES ("%s", "%s", "%s")' % (name_standard, url_standard, general_information))
+			con.commit()
+		else:
+			print ("such a standard exists")
+	except:
+		con.rollback()
+
+	con.close()
+
+def found_id_standard(url_standard):
+	con = database_connection(connectvars.connection())
+	cur = con.cursor()
+	try:
+		cur.execute('SELECT id FROM prof_standard WHERE url = "%s"' % url_standard)
+		results = cur.fetchone()
+	except:
+		print ("Error: unable to fetch data")
+
+	con.close()
+	return results
+
+def insert_information(name, id_information, function, text):
+	con = database_connection(connectvars.connection())
+	cur = con.cursor()
+	try:
+		if function == "labor_function" and text == "Требования к образованию и обучению":
+			cur.execute('INSERT education(name, id_function) VALUES ("%s", "%d")' % (name, id_information))
+			con.commit() 
+		elif function == "labor_function" and text == "Требования к опыту практической работы":
+			cur.execute('INSERT experience(name, id_function) VALUES ("%s", "%d")' % (name, id_information))
+			con.commit()
+		elif function == "subfunction" and text == "Трудовые действия":
+			cur.execute('INSERT labor_action(name, id_subfunctions) VALUES ("%s", "%d")' % (name, id_information))
+			con.commit()
+		elif function == "subfunction" and  text == "Требования к образованию и обучению":
+			cur.execute('INSERT necessary_knowledge(name, id_subfunctions) VALUES ("%s", "%d")' % (name, id_information))
+			con.commit()
+		elif function == "subfunction" and  text == "Требования к опыту практической работы":
+			cur.execute('INSERT necessary_skills(name, id_subfunctions) VALUES ("%s", "%d")' % (name, id_information))
+			con.commit()
+		elif function == "labor_function" and text == "Особые условия допуска к работе":
+			print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+		elif function == "labor_function":
+			cur.execute('INSERT labor_function(name, id_prof_standard) VALUES ("%s", "%d")' % (name, id_information))
+			con.commit() 
+		elif function == "subfunction":
+			cur.execute('INSERT subfunctions(name, id_function) VALUES ("%s", "%d")' % (name, id_information))
+			con.commit() 
+	except:
+		con.rollback()
+
+	con.close()
+
+def found_id_information(name, id_standard, function):
+	con = database_connection(connectvars.connection())
+	cur = con.cursor()
+	try:
+		if function == "labor_function":
+			cur.execute('SELECT id FROM labor_function WHERE name = "%s" and id_prof_standard = "%d"' % (name, id_standard))
+			results = cur.fetchone()
+		elif function == "subfunction":
+			cur.execute('SELECT id FROM subfunctions WHERE name = "%s" and id_function = "%d"' % (name, id_standard))
+			results = cur.fetchone()
+	except:
+		print ("Error: unable to fetch data")
+
+	con.close()
+	return results
+
+def url_prof():
+	con = database_connection(connectvars.connection())
+	cur = con.cursor()
+	try:
+		cur.execute('SELECT id, url FROM prof_standard')
+		results = cur.fetchall()
+	except:
+		print ("Error: unable to fetch data")
+
+	con.close()
+	return results
--- pro.py
+++ pro.py
@@ -5,13 +5,14 @@ from datetime import datetime
 import time
 import json
 import memory_profiler
+import database
 
 def get_html(url):
 	response = requests.get(url)
 	soup = BeautifulSoup(response.text, "html.parser")
 	return soup
 
-def labor_subfunction_competencies(url):
+def function_requirements(url):
 	soup = get_html('https://ppt.ru' + url)
 
 	array_competencies = {}
@@ -21,58 +22,63 @@ def labor_subfunction_competencies(url):
 		line_name = line_name.get_text()
 
 		competencies = clear_text(soup, line_name)
-		array_competencies[line_name] = competencies
+		if competencies:
+			array_competencies[line_name] = competencies
 
 	return array_competencies
 
 def clear_text(soup, text):
 	array = []
-	text = soup.find(text=text)
-	if text is not None:
-		for i in text.findNext().find_all("li"):
+	set_point = soup.find(text=text)
+	if set_point is not None:
+		for i in set_point.findNext().find_all("li"):
 			array.append(i.get_text())
 	else: array = "Не указано требований"
 	
 	return array
 
-def function_requirements(url):
-	soup = get_html(url)
-
-	education_title = "Требования к образованию и обучению"
-	experience_title = "Требования к опыту практической работы"
-
-	array_requirements = {
-	education_title: clear_text(soup, education_title),
-	experience_title: clear_text(soup, experience_title)
-	}
-
-	return array_requirements 
-
-def get_url_function(url):
+def get_url_function(url, id_standard):
 	subfunction_information, standard_information = [], []
 	item = 0
 
 	soup = get_html(url)
-	general_information = soup.find(class_="description")
 
 	for i in soup.find_all("tr"):
 		if i.find("td"):
 			if i.find("table"):
 				labor_function = i.find("a")
+				function = 'labor_function'
+				text =''
+				database.insert_information(labor_function.get_text(), id_standard, function, text)
 				subfunction_quantity = len(i.findAll("a")) - 1
-				function_requirement = function_requirements("https://ppt.ru" + labor_function.get('href'))
+				id_labor_function = database.found_id_information(labor_function.get_text(), id_standard, function)
+				function_requirement = function_requirements(labor_function.get('href'))
+				for key, value in function_requirement.items():
+					print(key)
+					for text in value:
+						database.insert_information(text, id_labor_function[0], function, key)
 				subfunction_information = []
 			else:
 				item += 1
 				labor_subfunction = i.find("a")
-				subfunction_competencies = labor_subfunction_competencies(labor_subfunction.get('href'))
+				function = "subfunction"
+				text =''
+				subfunction_competencies = function_requirements(labor_subfunction.get('href'))
 				subfunction_information.append({'name_labor_subfunction': labor_subfunction.get_text(), 'labor_subfunction': subfunction_competencies})
 				if subfunction_quantity == item:
 					standard_information.append({'name_labor_function': labor_function.get_text(), 'function_requirements': function_requirement, 'labor_subfunction': subfunction_information})
+					for k in subfunction_information:
+						database.insert_information(k['name_labor_subfunction'], id_labor_function[0], function, text)
+						id_labor_subfunction = database.found_id_information(k['name_labor_subfunction'], id_labor_function[0], function)
+						labor_subfunction = k['labor_subfunction']
+						for key, value in labor_subfunction.items():
+							print(key)
+							for text in value:
+								database.insert_information(text, id_labor_subfunction[0], function, key)
 					del subfunction_information
 					item = 0
 
-	return general_information.get_text(), standard_information
+	return standard_information
 
 def specialty_in_labor_function(url, specialty):
 	empty = 0 
@@ -119,27 +125,33 @@ def next_page(url):
 def prof_standard(url, specialty):
 	array_specialty_standard, array_found_standard = [], []
 	array_profstandarts_page = next_page(url)
-
+	k = 0
 	for x in array_profstandarts_page:
 		soup = get_html("https://ppt.ru/docs/"+ x)
 
 		for i in soup.find_all("a"):
 			if i.get("href") is not None and re.match(r'/docs/profstandarts', i.get("href")) is not None:
 				url = "https://ppt.ru"+ i.get("href")
+				k += 1
+				print(k)
 				found_standard = specialty_in_standard(url, specialty)
+
 				if found_standard != 0:
 					array_specialty_standard.append({'name_standard': i.get_text(), 'url_standard': url})
 
 	for item in array_specialty_standard:
-		soup = soup = get_html(item["url_standard"])
+		soup = get_html(item["url_standard"])
 		found_standard = []
+		general_information = soup.find(class_="description")
+
 		for i in soup.find_all("tr"):
 			if i.find("td"):
 				if i.find("table"):
 					labor_function = i.find("a")
 					found_standard = specialty_in_labor_function("https://ppt.ru" + labor_function.get('href'), specialty)
 					if found_standard != 0:
-						array_found_standard.append({'name_standard': item["name_standard"], 'url_standard': item["url_standard"]})
+						array_found_standard.append({'name_standard': item["name_standard"], 'url_standard': item["url_standard"], 'general_information': general_information.get_text()})
+						database.insert_prof_standard(item["name_standard"], item["url_standard"], general_information.get_text())
 						break
 
 	return array_found_standard
@@ -156,13 +168,15 @@ def main():
 	url_standard = "https://ppt.ru/docs/profstandarts"
 
 	array_standard = prof_standard(url_standard, specialty)
-	print(array_standard)
+
 	data = {}
 	k = 0
 	for i in array_standard:
-		general_information, standard_information = get_url_function(i["url_standard"])
-		data[k] = {"name_standard": i["name_standard"],"description": {"type_of_professional_activity": general_information, "labor_function": standard_information}}
 		k += 1
+		url_standard = i["url_standard"]
+		id_standard = database.found_id_standard(url_standard)
+		standard_information = get_url_function(url_standard, id_standard[0])
+		data[k] = {"name_standard": i["name_standard"],"description": {"type_of_professional_activity": general_information, "labor_function": standard_information}}
 
 	write_to_json(data)
 
